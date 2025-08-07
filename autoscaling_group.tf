447b8099cd67730a1a63e7a13744bd6dd717bff9
Folayemi <folayemiemoruwa@gmail.com>
2025-08-07 23:17:31 +0100

    Add files via upload

---
+++ autoscaling_group.tf
@@ -0,0 +1,21 @@
+resource "aws_autoscaling_group" "autoscaling" {
+  desired_capacity          = 2
+  max_size                  = 5
+  min_size                  = 1
+  health_check_type         = "ELB"
+  health_check_grace_period = 300
+
+  vpc_zone_identifier = aws_subnet.public[*].id
+  target_group_arns   = [aws_lb_target_group.alb-target-group.arn]
+
+  launch_template {
+    id      = aws_launch_template.launch_template.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "example-asg"
+    propagate_at_launch = true
+  }
+}
